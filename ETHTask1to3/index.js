27c2ada6ea0d71956dc90b98fcb7b75ce11224d8
Robert Kristian Alm <robert.alm0018@stud.hkr.se>
2022-01-08 02:40:42 +0100

    motion sensor on and off
    i will work with the display of the data when I am sure that it works.

--- ETHTask1to3/index.js
+++ ETHTask1to3/index.js
@@ -13,6 +13,9 @@ var BASE_URL = 'https://maker.ifttt.com/trigger/%s/with/key/%s';
 //HS100
 const { Client } = require('tplink-smarthome-api');
 const client = new Client();
+//Motion sensor motion.js
+require('console.table');
+var enabled;
 
 app.get('/', (req, res) => {
   let msg = pageGenerator("index", req, res); //generates a page dynamically.
@@ -39,6 +42,13 @@ app.get('/nettioonoff', (req, res) => {
 //Changing the color of the LED
 app.get('/toggleledcolor', (req, res) => {
   toggleLedColor();
+  let msg = pageGenerator("index", req, res); //generates a page dynamically.
+  res.write(msg);
+})
+
+//Changing the motion sensor on/off
+app.get('/motionsensor', (req, res) => {
+  activateMotion();
   let msg = pageGenerator("index", req, res); //generates a page dynamically.
   res.write(msg);
 })
@@ -75,9 +85,10 @@ function menuGenerator(req, res){
   msg2="";
   msg2 = msg2 + 'Status:' + res + '</br>';
   msg2 = msg2 + '<a href="/hs100onoff">1. Turn HS100 on/off</a></br>';
-  msg2 = msg2 + '<a href="/hs100onoff">1. Turn Nettio on/off</a></br>';
-  msg2 = msg2 + '<a href="/toggleledcolor">2. Toggle LED color</a></br>';
-  msg2 = msg2 + '<a href="/">3. Refresh the page</a></br>';
+  msg2 = msg2 + '<a href="/hs100onoff">2. Turn Nettio on/off</a></br>';
+  msg2 = msg2 + '<a href="/toggleledcolor">3. Toggle LED color</a></br>';
+  msg2 = msg2 + '<a href="/motionsensor">4. Toggle motion sensor</a></br>';
+  msg2 = msg2 + '<a href="/">5. Refresh the page</a></br>';
   return msg2;
 }
 
@@ -129,6 +140,69 @@ function toggleLedColor(){
 
 }
 
+function activateMotion(){
+  if (enabled) {
+    enabled = false;
+    this.tap_disable(function(error) {
+        console.log('Tap sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.orientation_disable(function(error) {
+        console.log('Orientation sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.quaternion_disable(function(error) {
+        console.log('Quaternion sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.stepCounter_disable(function(error) {
+        console.log('Step Counter sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.raw_disable(function(error) {
+        console.log('Raw sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.euler_disable(function(error) {
+        console.log('Euler sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.rotation_disable(function(error) {
+        console.log('Rotation sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.heading_disable(function(error) {
+        console.log('Heading sensor stopped! ' + ((error) ? error : ''));
+    });
+    this.gravity_disable(function(error) {
+        console.log('Gravity sensor stopped! ' + ((error) ? error : ''));
+    });
+}
+else {
+    enabled = true;
+    this.tap_enable(function(error) {
+        console.log('Tap sensor started! ' + ((error) ? error : ''));
+    });
+    this.orientation_enable(function(error) {
+        console.log('Orientation sensor started! ' + ((error) ? error : ''));
+    });
+    this.quaternion_enable(function(error) {
+        console.log('Quaternion sensor started! ' + ((error) ? error : ''));
+    });
+    this.stepCounter_enable(function(error) {
+        console.log('Step Counter sensor started! ' + ((error) ? error : ''));
+    });
+    this.raw_enable(function(error) {
+        console.log('Raw sensor started! ' + ((error) ? error : ''));
+    });
+    this.euler_enable(function(error) {
+        console.log('Euler sensor started! ' + ((error) ? error : ''));
+    });
+    this.rotation_enable(function(error) {
+        console.log('Rotation sensor started! ' + ((error) ? error : ''));
+    });
+    this.heading_enable(function(error) {
+        console.log('Heading sensor started! ' + ((error) ? error : ''));
+    });
+    this.gravity_enable(function(error) {
+        console.log('Gravity sensor started! ' + ((error) ? error : ''));
+    });
+}
+}
+
 //When we receive sensor data about gas from thingy52
 function onGasSensorData(gas) {
   console.log('Gas sensor: eCO2 ' + gas.eco2 + ' - TVOC ' + gas.tvoc )
@@ -155,6 +229,30 @@ function onDiscover(thingy) {
       console.log('Button enabled! ' + error);
     });
     thingy.on('gasNotif', onGasSensorData);
+    thingy.on('tapNotif', onTapData);
+    thingy.on('orientationNotif', onOrientationData);
+    thingy.on('quaternionNotif', onQuaternionData);
+    thingy.on('stepCounterNotif', onStepCounterData);
+    thingy.on('rawNotif', onRawData);
+    thingy.on('eulerNotif', onEulerData);
+    thingy.on('rotationNotif', onRotationData);
+    thingy.on('headingNotif', onHeadingData);
+    thingy.on('gravityNotif', onGravityData);
+    thingy.on('buttonNotif', onButtonChange);
+
+    thingy.motion_processing_freq_set(5, function(error) {
+        if (error) {
+            console.log('Motion freq set configure failed! ' + error);
+        }
+    });    
+
+    enabled = true;
+    thingy.orientation_enable(function(error) {
+        console.log('Orientation sensor started! ' + ((error) ? error : ''));
+    });
+    thingy.rotation_enable(function(error) {
+        console.log('Rotation sensor started! ' + ((error) ? error : ''));
+    });
     connectAndEnableGas(thingy);
   });
 }
@@ -220,5 +318,66 @@ process.argv.forEach(function(val, index, array){
         }
     }
 });
+
+//Motion sensor code
+console.log('Reading Thingy Motion sensors!');
+
+var Direction = Object.freeze([
+    'UNDEFINED',
+    'TAP_X_UP', 'TAP_X_DOWN',
+    'TAP_Y_UP', 'TAP_Y_DOWN',
+    'TAP_Z_UP', 'TAP_Z_DOWN'
+]);
+
+var Orientation = Object.freeze([
+    'Portrait', 'Landscape', 'Reverse portrait', 'Reverse landscape'
+]);
+
+function onTapData(tap) {
+    console.log('Tap data: Dir: %s (%d), Count: %d', 
+                        Direction[tap.direction], tap.direction, tap.count);
+}
+
+function onOrientationData(orientation) {
+    console.log('Orientation data: %s (%d)', Orientation[orientation], orientation);
+}
+
+function onQuaternionData(quaternion) {
+    console.log('Quaternion data: w: %d, x: %d, y: %d, z: %d', 
+        quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+}
+
+function onStepCounterData(stepCounter) {
+    console.log('Step Counter data: Steps: %d, Time[ms]: %d', 
+        stepCounter.steps, stepCounter.time);
+}
+
+function onRawData(raw_data) {
+    console.log('Raw data: Accelerometer: x %d, y %d, z %d', 
+        raw_data.accelerometer.x, raw_data.accelerometer.y, raw_data.accelerometer.z);
+    console.log('Raw data: Gyroscope: x %d, y %d, z %d', 
+        raw_data.gyroscope.x, raw_data.gyroscope.y, raw_data.gyroscope.z);
+    console.log('Raw data: Compass: x %d, y %d, z %d', 
+        raw_data.compass.x, raw_data.compass.y, raw_data.compass.z);
+}
+
+function onEulerData(euler) {
+    console.log('Euler angles: roll %d, pitch %d, yaw %d', 
+        euler.roll, euler.pitch, euler.yaw);
+}
+
+function onRotationData(rotation) {
+    console.log('Rotation: matrix:');
+
+    console.table(rotation);
+}
+
+function onHeadingData(heading) {
+    console.log('Heading: %d', heading);
+}
+
+function onGravityData(gravity) {
+    console.log('Gravity: x: %d, y %d, z %d', gravity.x, gravity.y, gravity.z);
+}
 
 Thingy.discover(onDiscover);
